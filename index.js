0ad668717e3a8df8722da30d8c34b5d1cc5363d8
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2021-12-06 01:29:59 +0500

    router and custom json file based database setup

--- index.js
+++ index.js
@@ -1,50 +1,82 @@
-/*
- * Primary file for the API
- *
- *
- */
-
-// Dependencies
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
+var fs = require('fs');
 
-// The server should respond to all requests with a string
-var server = http.createServer(function (req, res) {
-  // Get the URL and parse it
+var httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res);
+});
+
+httpServer.listen(config.httpPort, function () {
+  console.log(`The server is listening on port ${config.httpPort}`);
+});
+
+var httpsServerOptions = {
+  key: fs.readFileSync('./https/key.pem'),
+  cert: fs.readFileSync('./https/cert.pem'),
+};
+
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  unifiedServer(req, res);
+});
+
+httpsServer.listen(config.httpsPort, function () {
+  console.log(`The server is listening on port ${config.httpsPort}`);
+});
+
+var unifiedServer = function (req, res) {
   var parsedUrl = url.parse(req.url, true);
-
-  // Get the path
   var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-  // Get the query string as an object
   var queryStringObject = parsedUrl.query;
-
-  // Get the HTTP Method
   var method = req.method.toLowerCase();
-
-  // Get the headers as an object
   var headers = req.headers;
-
-  // Get the payload, if any
   var decoder = new StringDecoder('utf-8');
   var buffer = '';
+
   req.on('data', function (data) {
     buffer += decoder.write(data);
   });
+
   req.on('end', function () {
     buffer += decoder.end();
 
-    // Send the response
-    res.end('Hello World\n');
+    var chosenHandler =
+      typeof router[trimmedPath] !== 'undefined'
+        ? router[trimmedPath]
+        : handlers.notFound;
 
-    // Log the request path
-    console.log('Request received with this payload', buffer);
+    var data = {
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer,
+    };
+
+    chosenHandler(data, function (statusCode, payload) {
+      statusCode = typeof statusCode == 'number' ? statusCode : 200;
+      payload = typeof payload == 'object' ? payload : {};
+      var payloadString = JSON.stringify(payload);
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    });
   });
-});
+};
 
-// Start the server, and have it listen on port 3000
-server.listen(5000, function () {
-  console.log('The server is listening on port 5000 now');
-});
+var handlers = {};
+
+handlers.ping = function (data, callback) {
+  callback(200);
+};
+
+handlers.notFound = function (data, callback) {
+  callback(404);
+};
+
+var router = {
+  ping: handlers.ping,
+};
